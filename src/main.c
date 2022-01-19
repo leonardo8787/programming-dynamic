9b5a2378d9910ff232f2bbb987745f277541b036
leonardo8787 <leocampos8787@gmail.com>
2022-01-19 18:12:51 -0300

    atualizaçao

--- src/main.c
+++ src/main.c
@@ -18,22 +18,53 @@ int fibbonacci(int n) {
    }
 }
 
+int fibbonacci_topDown(int n) {
+   if(n == -1){
+      return (fibbonacci_topDown(n-1)+fibbonacci_topDown(n-2));
+   } else {
+      return n;
+   }
+}
+
 int main() {
    int n;
    int i;
+   int op;
 
    do{
       printf("\n\n---------------------------------\n");
-      printf("\n\nDigite o limite: \n");
-      scanf("%d", &n);
+      printf("\nDigite uma opção: \n");
+      printf("1 - Recurção simples\n");
+      printf("2 - Recursão topDown\n");
+      scanf("%d\n", &op);
       
-      printf("limite: %d\n", n);
-
-      printf("Factorial of %d: %d\n" , n , factorial(n));
-      printf("Fibbonacci of %d: " , n);
-      
-      for(i = 0;i<n;i++) {
-         printf("%d ",fibbonacci(i));            
+      switch (op)
+      {
+         case 1:
+            printf("\nRecursão\n");
+            printf("\n\nDigite o limite: \n");
+            scanf("%d", &n);
+            printf("limite: %d\n", n);
+            printf("Factorial of %d: %d\n" , n , factorial(n));
+            printf("Fibbonacci of %d: " , n);
+            for(i = 0;i<n;i++) {
+               printf("%d ",fibbonacci(i));            
+            }
+            break;
+         case 2:
+            printf("\nRecursão-TopDown\n");
+            printf("\n\nDigite o limite: \n");
+            scanf("%d", &n);
+            printf("limite: %d\n", n);
+            printf("Factorial of %d: %d\n" , n , factorial(n));
+            printf("Fibbonacci of %d: " , n);
+            for(i = 0;i<n;i++) {
+               printf("%d ",fibbonacci_topDown(i));            
+            }
+            break;
+         default:
+            printf("\nescolha uma opção válida!\n");
+            break;
       }
    }while(n != 0);
 
